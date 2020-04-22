7c87c71e40fd5ad18a06eb358e87302ace3549b5
Olga Jaeger <olgar.mamaeva@rambler.ru>
2020-04-22 16:43:03 +0200

    add session

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const exphbs = require("express-handlebars");
 const {
   allowInsecurePrototypeAccess,
 } = require("@handlebars/allow-prototype-access");
+const session = require('express-session');
 const homeRoutes = require("./routes/home");
 const addRoutes = require("./routes/add");
 const cardRoutes = require("./routes/card");
@@ -13,6 +14,7 @@ const authRoutes = require("./routes/auth");
 const ordersRouts = require("./routes/orders");
 const coursesRoutes = require("./routes/courses");
 const User = require("./models/user");
+const varMiddleware = require('./middleware/variables')
 
 const app = express();
 
@@ -26,18 +28,26 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
 
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findById("5e9596e1c25d3911722983ac");
-    req.user = user;
-    next();
-  } catch (e) {
-    console.log(e);
-  }
-});
+// app.use(async (req, res, next) => {
+//   try {
+//     const user = await User.findById("5e9596e1c25d3911722983ac");
+//     req.user = user;
+//     next();
+//   } catch (e) {
+//     console.log(e);
+//   }
+// });
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'some secret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(varMiddleware)
+
+
 
 app.use("/", homeRoutes);
 app.use("/add", addRoutes);
@@ -56,15 +66,15 @@ async function start() {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
-    const candidate = await User.findOne();
-    if (!candidate) {
-      const user = new User({
-        email: "test@mail.ru",
-        name: "Olga",
-        cart: { items: [] },
-      });
-      await user.save();
-    }
+    // const candidate = await User.findOne();
+    // if (!candidate) {
+    //   const user = new User({
+    //     email: "test@mail.ru",
+    //     name: "Olga",
+    //     cart: { items: [] },
+    //   });
+    //   await user.save();
+    // }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,25 @@
 const { Router } = require("express");
+const User = require('../models/user')
 const router = Router();
 
-router.get('/login', async (req, res) => {
-    res.render('auth/login', {
-        title: 'Авторизация',
-        isLogin: true
-    })
+router.get("/login", async (req, res) => {
+  res.render("auth/login", {
+    title: "Авторизация",
+    isLogin: true,
+  });
+});
 
-})
+router.get("/logout", async (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/auth/login#login");
+  });
+});
+
+router.post("/login", async (req, res) => {
+  const user = await User.findById("5e9596e1c25d3911722983ac");
+  req.session.user = user;
+  req.session.isAuthenticated = true;
+  await res.redirect("/");
+});
 
 module.exports = router;
