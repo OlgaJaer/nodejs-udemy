69f2aa7b8b0be5e10332f97579c0b7faf66e620d
Olga Jaeger <olgar.mamaeva@rambler.ru>
2020-04-28 12:03:55 +0200

    deploy

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require("path");
 const csrf = require("csurf");
 const flash = require("connect-flash");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
+const compression = require("compression");
 const Handlebars = require("handlebars");
 const exphbs = require("express-handlebars");
 const {
@@ -54,6 +56,8 @@ app.use(
 app.use(fileMiddleware.single("avatar")); //подключить до csrf
 app.use(csrf());
 app.use(flash()); //добавить после сессии
+app.use(helmet());
+app.use(compression());
 app.use(varMiddleware);
 app.use(userMiddleware);
 
